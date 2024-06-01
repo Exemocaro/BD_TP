6e77c9e5453c261215deaacfc2ffbcc98d542ebc
Bernard A. Georges <pg53698@alunos.uminho.pt>
2024-06-01 19:25:29 +0100

    trocas concordadas

--- MongoDB/queries.py
+++ MongoDB/queries.py
@@ -2,9 +2,9 @@ from pymongo import MongoClient
 import datetime
 
 
-#-> pedir o historico de um paciente
+# pedir o historico de um paciente
 def get_medical_history_by_id(patient_id, db):
-    collection = db['Patient']  # Replace with your actual collection name
+    collection = db['Patient']  #Replace with your actual collection name
 
     # Perform the query
     result = collection.find_one({'_id': patient_id}, {'_id': 0, 'MEDICAL_HISTORY': 1})
@@ -13,18 +13,31 @@ def get_medical_history_by_id(patient_id, db):
 
 
 def get_staff_related_records(staff_id, db):
-    collection = db['Staff']  # Replace with your actual Hospitalization collection name
+
+    collection = db['Department']  #Replace with your actual collection name
 
     pipeline = [
         {
             "$match": {
-                "_id": staff_id
+                "STAFF.STAFF_ID": staff_id
             }
+        },
+        {
+            "$project": {
+                "staff": {
+                    "$filter" : {
+                        "input": "$STAFF",
+                        "as": "staff",
+                        "cond": {  "$eq" : [ "$$staff.STAFF_ID", 14]}
+                    }
+                }
+            }
+
         },
         {
             "$lookup": {
                 "from": "Lab_Screening",  # The collection to join
-                "localField": "_id",  # Field from the current collection
+                "localField": "staff.STAFF_ID",  # Field from the current collection
                 "foreignField": "RESPONSIBLE_STAFF",  # Field from the Lab_Screening collection
                 "as": "Lab_Screenings"  # Output array field
             }
@@ -32,7 +45,7 @@ def get_staff_related_records(staff_id, db):
         {
             "$lookup": {
                 "from": "Appointment",  # The collection to join
-                "localField": "_id",  # Field from the current collection
+                "localField": "staff.STAFF_ID",  # Field from the current collection
                 "foreignField": "RESPONSIBLE_STAFF",  # Field from the Appointment collection
                 "as": "Appointments"  # Output array field
             }
@@ -40,15 +53,13 @@ def get_staff_related_records(staff_id, db):
         {
             "$lookup": {
                 "from": "Hospitalization",  # The collection to join
-                "localField": "_id",  # Field from the current collection
+                "localField": "staff.STAFF_ID",  # Field from the current collection
                 "foreignField": "RESPONSIBLE_STAFF",  # Field from the Appointment collection
                 "as": "Hospitalization"  # Output array field
             }
         }
-    ]
+        ]
 
-
-    # Aggregating data from Hospitalization collection
     results = list(collection.aggregate(pipeline))[0]
     
     return results['Lab_Screenings'] + results['Appointments'] + results['Hospitalization']
@@ -57,16 +68,16 @@ def get_staff_related_records(staff_id, db):
 
 #-> verificar todos os médicos de uma certa especialidade
 def get_doctors_by_specialty(specialty, db):
-    collection = db['Staff']  # Replace with your actual collection name
+    collection = db['Department']  #Replace with your actual collection name
 
     # Perform the query
-    result = list(collection.find({'DEPARTMENT.DEPT_NAME': specialty}))
+    result = list(collection.find({'DEPT_NAME': specialty}))
 
     return result
 
 #-> ver todas as medicines que o hospital já prescreveu para um paciente
 def get_all_prescribed_medicines(patient_id, db):
-    collection = db['Episode']  # Replace with your actual collection name
+    collection = db['Episode']  #Replace with your actual collection name
 
     # Perform the query
     result = list(collection.find({'PATIENT_ID': patient_id}, {'_id': 0, 'PRESCRIPTIONS.MEDICINE': 1}))
@@ -80,9 +91,9 @@ def get_all_prescribed_medicines(patient_id, db):
 
 #-> consultar as contas de um paciente
 def get_patient_bills(patient_id, db):
-    collection = db['Episode']  # Replace with your actual collection name
+    collection = db['Episode']  #Replace with your actual collection name
 
-    # Perform the query
+    #Perform the query
     result = list(collection.find({'PATIENT_ID': patient_id}, {'_id': 0, 'BILLS': 1}))
 
     ret = []
@@ -92,7 +103,7 @@ def get_patient_bills(patient_id, db):
 
     return ret
 
-#-> registar um novo episodio (funçao)
+# registar um novo episodio (funçao)
 def register_new_episode(patient_id, prescriptions, bills, db):
     collection = db['Episode']  # Replace with your actual collection name
 
@@ -101,36 +112,28 @@ def register_new_episode(patient_id, prescriptions, bills, db):
 
     return result
 
-#-> registar um novo paciente
-
-def register_new_patient(patient_name, blood_type, phone, email, gender, birthday, provider, insurance_plan, co_pay, covarage, maternity, dental, optical, emergency_contacts, db):
+# registar um novo paciente
+def register_new_patient(patient_name, blood_type, phone, email, gender, birthday, insurance_plan, emergency_contacts, db):
     collection = db['Patient']  # Replace with your actual collection name
     newPatient = {}
     nameSplit = patient_name.split(' ')
-    newPatient['FNAME'] = nameSplit[0]
-    newPatient['LNAME'] = nameSplit[-1]
-    newPatient['BLOOD_TYPE'] = blood_type
-    newPatient['PHONE'] = phone
-    newPatient['EMAIL'] = email
-    newPatient['GENDER'] = gender
-    newPatient['BIRTHDAY'] = datetime.datetime.strptime(birthday, '%Y-%m-%d')
-    newPatient['PROVIDER'] = provider
-    newPatient['INSURANCE_PLAN'] = insurance_plan
-    newPatient['CO_PAY'] = co_pay
-    newPatient['COVERAGE'] = covarage
-    newPatient['MATERNITY'] = maternity
-    newPatient['DENTAL'] = dental
-    newPatient['OPTICAL'] = optical
+    newPatient["PATIENT_FNAME"] = nameSplit[0]
+    newPatient["PATIENT_LNAME"] = nameSplit[-1]
+    newPatient["BLOOD_TYPE"] = blood_type
+    newPatient["PHONE"] = phone
+    newPatient["EMAIL"] = email
+    newPatient["GENDER"] = gender
+    newPatient["POLICY"] = insurance_plan
+    newPatient["BIRTHDAY"] = birthday
     newPatient['EMERGENCY_CONTACT'] = emergency_contacts
     newPatient['MEDICAL_HISTORY'] = []
-
 
     # Perform the query
     result = collection.insert_one(newPatient)
 
     return result
 
-#-> verificar todos os quartos de hospital onde um enfermeiro já operou
+
 #-> update do contacto de emergencia do paciente
 def update_emergency_contact(patient_id, new_contact_list, db):
     collection = db['Patient']  # Replace with your actual collection name
@@ -144,9 +147,10 @@ def update_emergency_contact(patient_id, new_contact_list, db):
 # Replace the URI string with your MongoDB deployment's connection string.
 client = MongoClient("mongodb://localhost:27017/")
 db = client['Projeto'] 
-#print(get_staff_related_records(14, db))
+print(get_staff_related_records(14, db))
 #print(get_medical_history_by_id(1, db))
 #print(get_doctors_by_specialty('Cardiology', db))
 #print(get_all_prescribed_medicines(1, db))
 #print(get_patient_bills(3, db))
-update_emergency_contact(1, [{'CONTACT_NAME' : "John Doe", "RELATION": "Father", "PHONE": "111-222-3333"}], db)
+print(register_new_patient("John Doe", "O-", "111-222-3333", "doe@mail.com", "M", datetime.datetime(1990, 1, 1), "POL004", [], db))
+#update_emergency_contact(1, [{'CONTACT_NAME' : "John Doe", "RELATION": "Father", "PHONE": "111-222-3333"}], db)
